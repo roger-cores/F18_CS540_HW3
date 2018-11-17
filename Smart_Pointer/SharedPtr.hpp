9e6b6c05da962fcd123cc5387ccda8c3245a4144
roger-cores <rcorrei1@binghamton.edu>
2018-11-17 03:23:16 -0500

    added modifiers and casts

--- Smart_Pointer/SharedPtr.hpp
+++ Smart_Pointer/SharedPtr.hpp
@@ -23,6 +23,9 @@ namespace cs540 {
     int get_count() {
       return count;
     }
+    T* get() const {
+      return s_ptr;
+    }
   }
   template<typename T>
   class SharedPtr {
@@ -111,8 +114,122 @@ namespace cs540 {
     }
 
     // ------------------------------- Observers  -------------------------------\
-    
+    // Returns a pointer to owned Object
+    T *get() const {
+      if(ref_count != NULL) return ref_count->get();
+      else return NULL;
+    }
 
+    // Returns a reference to the owned object
+    T &opearator*() const {
+      return *(ref_count->get());
+    }
+
+    // Returns a pointer to owned Object
+    T *operator->() const {
+      if(ref_count != NULL) return ref_count->get();
+      return NULL;
+    }
+
+    // Returns true if the SharedPtr is not null
+    explicit operator bool() const {
+      if(ref_count != NULL && ref_count->get() != NULL) return true;
+      else return false;
+    }
 
   };
+
+  // -------------------------------  Non-member Functions ----------------------
+
+  // Compare two RefCounts with different (or same) template params
+  // Necessary for == between SharedPtrS
+  template<typename T1, typename T2>
+  bool opearator==(const RefCount<T1> &r1, const RefCount<T2> &r2) {
+    return r1.s_ptr == r2.s_ptr;
+  }
+
+  // Compare two SharedPtr with different (or same) template params
+  template<typename T1, typename T2>
+  bool operator==(const SharedPtr<T1> &p1, const SharedPtr<T2> &p2) {
+    return p1 == p2;
+  }
+
+  // Next four functions are for comparisons with nullptr
+  template<typename T>
+  bool opearator==(const RefCount<T> &r, std::nullptr_t) {
+    return r1.s_ptr == NULL;
+  }
+
+  template<typename T>
+  bool opearator==(std::nullptr_t, const RefCount<T> &r) {
+    return r1.s_ptr == NULL;
+  }
+
+  template<typename T>
+  bool opearator==(const SharedPtr<T> &p, std::nullptr_t) {
+    return p.ref_count == NULL;
+  }
+
+  template<typename T>
+  bool opearator==(std::nullptr_t, const SharedPtr<T> &p) {
+    return p.ref_count == NULL;
+  }
+
+  // Compare two RefCounts with different (or same) template params for inequality
+  // Necessary for != between SharedPtrS
+  template<typename T1, typename T2>
+  bool opearator!=(const RefCount<T1> &r1, const RefCount<T2> &r2) {
+    return r1.s_ptr != r2.s_ptr;
+  }
+
+  // Compare two SharedPtr with different (or same) template params for inequality
+  template<typename T1, typename T2>
+  bool operator!=(const SharedPtr<T1> &p1, const SharedPtr<T2> &p2) {
+    return p1 != p2;
+  }
+
+  // Next four functions are for inquality check with nullptr
+  template<typename T>
+  bool opearator!=(const RefCount<T> &r, std::nullptr_t) {
+    return r1.s_ptr != NULL;
+  }
+
+  template<typename T>
+  bool opearator!=(std::nullptr_t, const RefCount<T> &r) {
+    return r1.s_ptr != NULL;
+  }
+
+  template<typename T>
+  bool opearator!=(const SharedPtr<T> &p, std::nullptr_t) {
+    return p.ref_count != NULL;
+  }
+
+  template<typename T>
+  bool opearator!=(std::nullptr_t, const SharedPtr<T> &p) {
+    return p.ref_count != NULL;
+  }
+
+  // Static pointer cast from RefCount of type U to T
+  template <typename T, typename U>
+  RefCount<T> static_pointer_cast(const RefCount<U> &sp) {
+    return RefCount<T>(static_cast<T*>(sp.get()));
+  }
+
+  // Static pointer cast from SharedPtr of type U to T
+  template <typename T, typename U>
+  SharedPtr<T> static_pointer_cast(const SharedPtr<U> &sp) {
+    return SharedPtr<T>(static_cast<T*>(sp.ref_count));
+  }
+
+  // Dynamic cast
+  template <typename T, typename U>
+  RefCount<T> dynamic_pointer_cast(const RefCount<U> &sp) {
+    // TODO
+  }
+
+  template <typename T, typename U>
+  SharedPtr<T> dynamic_pointer_cast(const SharedPtr<U> &sp) {
+    // TODO
+  }
+
 }
