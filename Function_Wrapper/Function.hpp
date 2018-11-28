f75c554c69b95e4e10ee05c6b88e1c0298565e61
roger-cores <rcorrei1@binghamton.edu>
2018-11-27 22:12:45 -0500

    added callable

--- Function_Wrapper/Function.hpp
+++ Function_Wrapper/Function.hpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
 namespace cs540 {
+
+  template<typename ResultType, typename ... ArgumentTypes>
+  class CallableAbstract<ResultType(ArgumentTypes...)> {
+    virtual ~CallableAbstract() = default;
+    virtual ResultType operator()(ArgumentTypes... args) = 0;
+    virtual CallableAbstract<ResultType(ArgumentTypes...)>* clone() = 0;
+  };
+
+  template <typename FunctionType, typename ResultType, typename ... ArgumentTypes>
+  class Callable : public CallableAbstract<ResultType(ArgumentTypes...)> {
+  private:
+    FunctionType callable;
+  public:
+    Callable(const FunctionType& callable) : callable(callable) {}
+
+    virtual CallableAbstract<ResultType(ArgumentTypes...)>* clone() override {
+      return new Callable<FunctionType, ResultType, ArgumentTypes...>(callable);
+    }
+
+    virtual ResultType operator()(ArgumentTypes... args) override {
+      return callable(std::forward<ArgumentTypes>(args)...);
+    }
+
+    virtual ~Callable() override = default;
+
+  };
+
   template <typename ResultType, typename ... ArgumentTypes>
   class Function<ResultType(ArgumentTypes ...)> {
   public:
